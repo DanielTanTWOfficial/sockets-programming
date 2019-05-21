4c9551dc562c4dd21fc823f392c535a9461d2b26
DanielTanTWOfficial <Danieltantwofficial@gmail.com>
2019-05-21 19:01:32 +0800

    upgrades done

--- server.c
+++ server.c
@@ -16,6 +16,7 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
     char *hello = "Hello from the server";
+    char *wrong = "Wrong message";
 
     // Creating socket file descriptor
     if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -58,7 +59,15 @@ int main(int argc, char const *argv[])
 
     valread = read(new_socket, buffer, 1024);
     printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
+    // check if message from client is 'Hello from client'
+    if(strcmp(buffer, hello)){
+        send(new_socket, hello, strlen(hello), 0);
+        printf("Hello message sent\n");
+    } else {
+        send(new_socket, wrong, strlen(wrong), 0);
+        printf("Wrong message received\n");
+    }
+    // send(new_socket, hello, strlen(hello), 0);
+    // printf("Hello message sent\n");
     return 0;
 }
