09dbd5f9da677b6739d75ea51c92387a2dc8f9a0
DanielTanTWOfficial <Danieltantwofficial@gmail.com>
2019-05-21 15:46:21 +0800

    fixed errors

--- server.c
+++ server.c
@@ -20,7 +20,7 @@ int main(int argc, char const *argv[])
     // Creating socket file descriptor
     if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
-        peror("socket failed");
+        perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
@@ -28,15 +28,15 @@ int main(int argc, char const *argv[])
     if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                                                 &opt, sizeof(opt)))
     {
-        peror("setsockopt");
+        perror("setsockopt");
         exit(EXIT_FAILURE);
     }
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADR_ANY;
+    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
 
     // Forcefully attaching socket to port 8080
-    if(bind(derver_fd, (struct sockaddr *)&address,
+    if(bind(server_fd, (struct sockaddr *)&address,
                                 sizeof(address))<0)
     {
         perror("bind failed");
